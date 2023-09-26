6b3a09cbc554179bbe15f8fb0447c454c6b862ec
sagarshelke744 <107041922+sagarshelke744@users.noreply.github.com>
2023-09-26 11:45:48 +0000

    math and num

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -7,3 +7,36 @@
 // Refrence type (Non primitive)
 
 //Array, Objects, Functions
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++
+
+
+// stack (primitive) -makes a decleared variable copy
+// heap(Non-primitive)- gets refrence thats why  changes in original value
+
+
+//Methods of String
+
+
+// const gameName= String("sagar")
+
+// const newstring =gameName.split('');
+//  const st=newstring.reverse(" ")
+//  const sr=st.join(" ")
+// console.log(sr);
+
+
+//+++++++++++++++++++++++++ MATH +++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.max(1,2,7,4));
+
+
+
+// console.log(Math.floor(Math.random()*10)+1);
+
